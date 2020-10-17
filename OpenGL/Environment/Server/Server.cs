d2da8901c1dfa2f782b4fa61385755463c0ccdff
Dmitri <56200546+Syritx@users.noreply.github.com>
2020-10-17 10:49:44 -0400

    changed server

--- OpenGL/Environment/Server/Server.cs
+++ OpenGL/Environment/Server/Server.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace OpenGL.Environment.Server
@@ -15,12 +16,20 @@ namespace OpenGL.Environment.Server
         static ClientCommands clientCommands = new ClientCommands();
         static int clientId = -1;
 
+        static int playerCount = 10;
+        static bool[] places;
+
         static IPAddress ip = Dns.GetHostEntry(Dns.GetHostName()).AddressList[0];
         static int port = 5050;
 
         public static void Main(string[] args) {
             Console.Title = "[SERVER PANEL]";
             Console.WriteLine("[SERVER PANEL]");
+
+            places = new bool[playerCount];
+            for (int i = 0; i < playerCount; i++){
+                places[i] = false;
+            }
 
             clients = new List<Socket>();
             server = new Socket(AddressFamily.InterNetwork,
@@ -35,8 +44,62 @@ namespace OpenGL.Environment.Server
             ReceiveClients();
         }
 
+        static void RemoveClientFromServer(Socket client, int id) {
+            client.Close();
+            clients.Remove(client);
+            places[id-1] = false;
+
+            Console.WriteLine("Client {0} has disconnected from the server",id);
+        }
+
+        static void ReceiveAndSendMessages(Socket client, string idString, int id) {
+
+            IPEndPoint clientEndPoint = client.RemoteEndPoint as IPEndPoint;
+            byte[] bytes = new byte[2048];
+
+            while (client.Connected) {
+                bytes = new byte[2048];
+                int i = client.Receive(bytes);
+
+                string command = Encoding.UTF8.GetString(bytes);
+                Console.WriteLine("[{0}, {1}] SENT: {2}", clientEndPoint.Address,
+                                                    clientEndPoint.Port,
+                                                    command);
+                string newCommand = idString + "-CMD-" + command;
+
+                if (client.Connected) {
+                    // SENDING COMMANDS
+                    foreach(Socket clientSocket in clients) {
+                        if (clientSocket != client) {
+                            try {
+                                byte[] idBytes = Encoding.UTF8.GetBytes(newCommand);
+                                clientSocket.Send(idBytes);
+                            }
+                            catch (Exception e) {}
+                        }
+                    }
+                }
+            }
+        }
+
+        static void DetectClientStatus(Socket client, int clientID) {
+            while (client != null) {
+                if (!client.Connected) RemoveClientFromServer(client, clientID);
+                Thread.Sleep(10);
+            }
+        }
+
         static void clientThread(Socket client) {
-            int currentClientId = clientId;
+            int currentClientId = 0;
+
+            for (int i = 0; i < places.Length; i++) {
+                if (!places[i]) {
+                    currentClientId = (i + 1);
+                    places[i] = true;
+                    break;
+                }
+            }
+            if (currentClientId == 0) RemoveClientFromServer(client, 0);
 
             string message = "ID: " + currentClientId.ToString()+ " ";
             string idString = "ID: " + currentClientId.ToString();
@@ -45,38 +108,10 @@ namespace OpenGL.Environment.Server
             byte[] bytes = new byte[2048];
 
             client.Send(messageBytes);
-            Console.WriteLine("sent message to client");
-            IPEndPoint clientEndPoint = client.RemoteEndPoint as IPEndPoint;
+            ClientConnected(client, idString);
 
-            string newClientMessage = "[CLIENT_CREATED]: " + idString;
-            foreach (Socket socketClient in clients)
-            {
-                byte[] newClient = Encoding.UTF8.GetBytes(newClientMessage);
-                if (socketClient != client) socketClient.Send(newClient);
-            }
-
-            while (client.Connected) {
-                bytes = new byte[2048];
-                int i = client.Receive(bytes);
-
-                string command = Encoding.UTF8.GetString(bytes);
-                Console.WriteLine("[{0}, {1}] SENT: {2}",clientEndPoint.Address,
-                                                    clientEndPoint.Port,
-                                                    command);
-                string newCommand = idString + "-CMD-" + command;
-
-                // SENDING ID
-                foreach(Socket clientSocket in clients) {
-                    if (clientSocket != client) {
-                        try {
-                            byte[] idBytes = Encoding.UTF8.GetBytes(newCommand);
-                            clientSocket.Send(idBytes);
-                        }
-                        catch (Exception e) { clients.Remove(clientSocket); }
-                    }
-                }
-            }
-            clients.Remove(client);
+            Task.Run(() => ReceiveAndSendMessages(client, idString, currentClientId));
+            Task.Run(() => DetectClientStatus(client, currentClientId));
         }
 
         static void ReceiveClients() {
@@ -87,6 +122,23 @@ namespace OpenGL.Environment.Server
                 clients.Add(client);
                 Task.Run(() => clientThread(client));
             }
+        }
+
+        // CLIENT METHODS
+        static void ClientConnected(Socket client, string idString) {
+
+            // sending to all clients a notification
+            string newClientMessage = "[CLIENT_CREATED]: " + idString;
+            foreach (Socket socketClient in clients)
+            {
+                byte[] newClient = Encoding.UTF8.GetBytes(newClientMessage);
+                if (socketClient != client) socketClient.Send(newClient);
+            }
+            return;
+        }
+
+        static void ClientDisconnected() {
+
         }
     }
 }
